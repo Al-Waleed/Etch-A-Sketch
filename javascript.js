9f3e79333e05224f00bf89347b99e9cf7860815d
waleed <waleedtariq47@yahoo.com>
2023-01-14 17:02:00 +0300

    make the grid creator function erase its content each time a new grid is selected

--- javascript.js
+++ javascript.js
@@ -2,48 +2,19 @@ const container = document.getElementById("container");
 
 const getGridsValue = document.getElementById("grids");
 getGridsValue.addEventListener("change", () => createGrids(getGridsValue.value))
-
+createGrids(8)
 function createGrids(grid){
+    container.innerHTML = ""
     container.style.setProperty('--grid-rows', grid);
     container.style.setProperty("--grid-cols", grid);
     for (i = 0; i < (grid * grid); i++){
         let div = document.createElement("div");
         container.appendChild(div).className = "grid";
+        div.addEventListener("mouseover", paint);
     };
 }
 
+function paint(e) {
+    e.target.style.backgroundColor = 'black';
+  }
 
-
-//const container = document.getElementById("container");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const getGridsValue = document.querySelector("#grids")
-//getGridsValue.addEventListener("change", () => { 
-//
-//    const grids = document.querySelector(".container")
-//    console.log(getGridsValue.value)
-//    for (let i = 0; i < getGridsValue.value ; i++) {
-//        grids.innerHTML += "<div class='grid'>"   
-//    }
-//});
