f5e675e3f03376bd0ad01eaf6fb70ab6d48b6dd2
waleed <waleedtariq47@yahoo.com>
2023-01-15 13:21:13 +0300

    Add clear button function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 const container = document.getElementById("container");
 
 const getGridsValue = document.getElementById("grids");
-getGridsValue.addEventListener("change", () => createGrids(getGridsValue.value))
-createGrids(8)
+getGridsValue.addEventListener("change", () => createGrids(getGridsValue.value));
+createGrids(8);
 function createGrids(grid){
     container.innerHTML = ""
     container.style.setProperty('--grid-rows', grid);
@@ -10,11 +10,27 @@ function createGrids(grid){
     for (i = 0; i < (grid * grid); i++){
         let div = document.createElement("div");
         container.appendChild(div).className = "grid";
-        div.addEventListener("mouseover", paint);
+        div.addEventListener("click", paint);
     };
-}
+};
 
 function paint(e) {
-    e.target.style.backgroundColor = 'black';
-  }
+    e.target.style.backgroundColor = 'red';
+    };
+//const isClicked = document.getElementById(#grid)///////continue here
+//if (isClicked === true){
+//
+//};
 
+//container.addEventListener("onmousedown", function onclick(event) {
+//    container.addEventListener("mouseover", () => { event.target.style.backgroundColor = "red" })
+//    console.log(event.target);
+//    //event.target.style.backgroundColor = "red";
+//});
+
+const clearBtn = document.querySelector(".clear");
+
+clearBtn.addEventListener("click", () => { // clear btn that empties the container and adds the chosen num of grids to it
+    container.innerHTML = "";
+    createGrids(getGridsValue.value);
+});
